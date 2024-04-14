979f59c815f8fb34d224396d2409e4e1de374933
laof <345603828@qq.com>
2024-04-15 01:11:41 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import "./App.css";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { store } from "./store/index";
 import { User } from "./User";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+
+import { useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Navigation } from "swiper/modules";
 
 function App() {
   return (
     <Provider store={store}>
       <header>header</header>
       <User></User>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={0}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={true}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="my-test">fdsfaf 1222 </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="my-test">fdsfaf 4444</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="my-test">fdsfaf 5555</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="my-test">fdsfaf 666</div>
+        </SwiperSlide>
+      </Swiper>
     </Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
